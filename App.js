e8c3ddfb3b108b25587ecf56647c2d98886da6d0
jiaomeizhou <zhoujiaomei@outlook.com>
2024-02-07 16:07:51 -0800

    Add four empty screens and update App.js

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react';
+import Start from './screens/Start'
+import AddAnActivity from './screens/AddAnActivity'
+import SpecialActivities from './screens/SpecialActivities'
+import AllActivities from './screens/AllActivities'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="AddAnActivity" component={AddAnActivity} />
+        <Stack.Screen name="SpecialActivities" component={SpecialActivities} />
+        <Stack.Screen name="AllActivities" component={AllActivities} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Start.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Start() {
+  return (
+    <View>
+      <Text>start</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
