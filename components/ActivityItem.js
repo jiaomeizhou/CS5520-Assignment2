0cacf9c8a5033b8e013389867c5bebbb69683999
jiaomeizhou <zhoujiaomei@outlook.com>
2024-02-10 16:14:56 -0800

    show warning sign for special activities

--- components/ActivityItem.js
+++ components/ActivityItem.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Entypo } from '@expo/vector-icons';
 
 export default function ActivityItem({ activityObj }) {
     return (
         <View>
             <Text>{activityObj.name}</Text>
-            <Text>{activityObj.type}</Text>
+            {activityObj.isSpecial && <Entypo name="warning" size={24} color="black" />}
             <Text>{activityObj.date ? activityObj.date.toDateString() : ''}</Text>
             <Text>{activityObj.duration}</Text>
         </View>
