b73174c8e28aa870c79837d100d73604cd11c3dc
jiaomeizhou <zhoujiaomei@outlook.com>
2024-02-25 12:45:18 -0800

    replace add button with an icon, add PressableButton component

--- components/AddActivityButton.js
+++ components/AddActivityButton.js
@@ -1,13 +1,18 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React, { useEffect } from 'react'
 import * as Colors from './Color'
+import { Ionicons } from '@expo/vector-icons';
+import PressableButton from './PressableButton';
 
 // This component is a button to navigate to AddAnActivity screen
 export default function AddActivityButton({ navigation }) {
 
   return (
     <View>
-      <Button title="Add" onPress={() => navigation.navigate('AddAnActivity')} color={Colors.focusColorYellow} />
+      <PressableButton onPress={() => navigation.navigate('AddAnActivity')} >
+        <Ionicons name="add" size={24} color={Colors.primaryWhiteColor}/>
+      </PressableButton>
+      {/* <Button title="Add" onPress={() => navigation.navigate('AddAnActivity')} color={Colors.focusColorYellow} /> */}
     </View>
   )
 }
